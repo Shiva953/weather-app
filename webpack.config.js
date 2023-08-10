7bb7fdb60f95d1f64cf3dfb5132160c4001f2924
Shiva953 <b22070@students.iitmandi.ac.in>
2023-08-10 20:53:42 +0530

    dist->docs

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   mode: 'development',
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'docs'),
   },
   plugins: [
     new Dotenv(), // Use dotenv-webpack to load environment variables from .env
